aa4fc869bcedcf5ab3575b30ebbe3d9ab0098d0d
Gregg B. Jensen <greggbjensen@ldschurch.org>
2017-07-14 12:10:37 -0600

    Completed columns and rows as well as placements.  Updated package.json with information.

--- index.js
+++ index.js
@@ -1,13 +1,62 @@
 var postcss = require('postcss');
 
-module.exports = postcss.plugin('PLUGIN_NAME', function (opts) {
-    opts = opts || {};
+module.exports = postcss.plugin('postcss-css-grid-ie', function () {
 
-    // Work with options here
+    const columnRowValueRegEx = /(\d+)\s*\/\s*(\d+)/;
 
-    return function (root, result) {
+    function processDisplay(decl, rule) {
 
-        // Transform CSS AST here
+        // Add display: -ms-grid;
+        if (decl.value.toLowerCase() === 'grid') {
+            rule.insertBefore(decl,
+                postcss.decl({ prop: 'display', value: '-ms-grid' }));
+        }
+    }
 
+    function processGridColumnsRows(decl) {
+        decl.cloneAfter({ prop: `-ms-${decl.prop}` });
+    }
+
+    function processGridColumnRow(decl, rule, columnRow) {
+        const match = columnRowValueRegEx.exec(decl.value);
+        if (match) {
+            const place = match[1];
+            const span = match[2];
+
+            rule.insertAfter(decl,
+                postcss.decl(
+                    { prop: `-ms-grid-${columnRow}`, value: place }));
+
+            rule.insertAfter(decl.next(),
+                postcss.decl(
+                    { prop: `-ms-grid-${columnRow}-span`, value: span }));
+        }
+    }
+
+    return (css) => {
+
+        css.walkRules((rule) => {
+
+            rule.walkDecls((decl) => {
+
+                switch (decl.prop.toLowerCase()) {
+                case 'display':
+                    processDisplay(decl, rule);
+                    break;
+                case 'grid-columns':
+                case 'grid-rows':
+                    processGridColumnsRows(decl);
+                    break;
+                case 'grid-column':
+                    processGridColumnRow(decl, rule, 'column');
+                    break;
+                case 'grid-row':
+                    processGridColumnRow(decl, rule, 'row');
+                    break;
+                default:
+                    // Do nothing.
+                }
+            });
+        });
     };
 });
--- index.test.js
+++ index.test.js
@@ -10,10 +10,68 @@ function run(input, output, opts) {
         });
 }
 
-/* Write tests here
+describe('postcss-css-grid-ie', () => {
 
-it('does something', () => {
-    return run('a{ }', 'a{ }', { });
+    it('adds display: -ms-grid;', () => {
+        return run(
+            `.container { 
+                display: grid;
+            }`,
+
+            `.container { 
+                display: -ms-grid; 
+                display: grid;
+            }`, { });
+    });
+
+    it('adds -ms-grid-columns', () => {
+        return run(
+            `.container-item { 
+                grid-columns: 20px 1fr auto;
+            }`,
+
+            `.container-item { 
+                grid-columns: 20px 1fr auto; 
+                -ms-grid-columns: 20px 1fr auto;
+            }`, { });
+    });
+
+    it('adds -ms-grid-rows', () => {
+        return run(
+            `.container-item { 
+                grid-rows: 20px 1fr auto;
+            }`,
+
+            `.container-item { 
+                grid-rows: 20px 1fr auto; 
+                -ms-grid-rows: 20px 1fr auto;
+            }`, { });
+    });
+
+    it('adds -ms-grid-column and -ms-grid-column-span', () => {
+        return run(
+            `.container-item { 
+                grid-column: 2 / 3;
+            }`,
+
+            `.container-item { 
+                grid-column: 2 / 3; 
+                -ms-grid-column: 2; 
+                -ms-grid-column-span: 3;
+            }`, { });
+    });
+
+    it('adds -ms-grid-row and -ms-grid-row-span', () => {
+        return run(
+            `.container-item { 
+                grid-row: 3 / 1;
+            }`,
+
+            `.container-item { 
+                grid-row: 3 / 1; 
+                -ms-grid-row: 3; 
+                -ms-grid-row-span: 1;
+            }`, { });
+    });
 });
 
-*/
